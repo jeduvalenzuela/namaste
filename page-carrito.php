cec393a70863dc1e58804136b7644f0dcb980e8c
J. Eduardo Valenzuela <j.eduvalenzuela@gmail.com>
2024-12-10 07:36:33 -0300

    loop of cart

--- page-carrito.php
+++ page-carrito.php
@@ -77,13 +77,6 @@ Cart Area
 
 <tr>
                             <td colspan="6" class="actions">
-                                <?php if ( wc_coupons_enabled() ) { ?>
-                                    <div class="coupon">
-                                        <label for="coupon_code"><?php esc_html_e( 'Coupon:', 'woocommerce' ); ?></label>
-                                        <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" />
-                                        <button type="submit" class="button" name="apply_coupon"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-                                    </div>
-                                <?php } ?>
                                 <button type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
                                 <?php wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ); ?>
                             </td>
